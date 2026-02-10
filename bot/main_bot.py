b1f2b1d97b37f808957508bcf3b9ab050eec2772
rezajavadi995 <rezajavadi995@gmail.com>
2026-02-11 00:35:10 +0330

    Update main_bot.py

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,14 +1,20 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackQueryHandler
+import asyncio
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from config import BOT_TOKEN
 from bot.handlers import start, handle_message, handle_callback
 
-updater = Updater(BOT_TOKEN)
+async def main():
+    # ساخت ربات
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(filters.text & ~filters.command, handle_message))
-dp.add_handler(CallbackQueryHandler(handle_callback))
+    # ثبت handler ها
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(CallbackQueryHandler(handle_callback))
 
-print("✅ ربات در حال اجراست")
-updater.start_polling()
-updater.idle()
+    print("✅ ربات در حال اجراست")
+    await app.start_polling()
+    await app.idle()
+
+if __name__ == "__main__":
+    asyncio.run(main())
