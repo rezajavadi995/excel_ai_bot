3cecdbd3aaa35952140be3fa3278057b08a8da81
rezajavadi995 <rezajavadi995@gmail.com>
2026-02-11 00:30:31 +0330

    Update main_bot.py

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from config import BOT_TOKEN
 from bot.handlers import start, handle_message, handle_callback
 
@@ -6,7 +6,7 @@ updater = Updater(BOT_TOKEN)
 
 dp = updater.dispatcher
 dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+dp.add_handler(MessageHandler(filters.text & ~filters.command, handle_message))
 dp.add_handler(CallbackQueryHandler(handle_callback))
 
 print("✅ ربات در حال اجراست")
